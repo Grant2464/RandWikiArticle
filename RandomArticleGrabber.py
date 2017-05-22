b63038545da638b77018c26847777ff7393703af
Grant2464 <grantpauker@gmail.com>
2017-05-21 19:19:36 -0700

    Add files via upload

---
+++ RandomArticleGrabber.py
@@ -0,0 +1,37 @@
+import requests
+import urllib.request
+
+import wikipedia
+import urllib.parse
+import io
+from sys import argv
+
+
+def article(category):
+    if (category=="All"):
+        response = requests.get("https://en.wikipedia.org/wiki/Special:Random")
+    else:
+        response = requests.get("https://en.wikipedia.org/wiki/Special:RandomInCategory/" + category)
+    if response.history:
+        print (response.url)
+    else:
+        print ("Request was not redirected")
+    url = urllib.parse.unquote(response.url)
+    url = url.replace("https://en.wikipedia.org/wiki/","")
+    url = url.replace("_"," ")
+    wiki = wikipedia.page(url)
+    print(wiki.title)
+    #All available functions
+        #wiki.title - Get title of article
+        #wiki.url - Get url of article
+        #wiki.content - content title of article
+        #wiki.links[0] - Get links of article
+    target = io.open("Article.txt", 'w' , encoding='utf8')
+    target.write(wiki.title)
+    target.write("\n \n")
+    target.write(wiki.content)
+    target.close()
+    return
+article("All") #List of categories at: https://en.wikipedia.org/wiki/Portal:Contents/Categories
+
+
